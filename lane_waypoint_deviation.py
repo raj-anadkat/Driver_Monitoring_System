eef74ca1868f7ef99583adc8729488966e96eeae
Raj Anadkat <109377585+raj-anadkat@users.noreply.github.com>
2023-08-14 05:13:31 -0400

    added impairment detections

--- lane_waypoint_deviation.py
+++ lane_waypoint_deviation.py
@@ -4,6 +4,8 @@ import glob
 import os
 import sys
 import numpy as np
+import time
+import pygame
 
 IM_WIDTH = 1280
 IM_HEIGHT = 720
@@ -17,7 +19,6 @@ except IndexError:
     pass
 
 import carla
-import pygame
 
 def process_img(image):
     array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
@@ -55,8 +56,20 @@ pygame.init()
 screen = pygame.display.set_mode((IM_WIDTH, IM_HEIGHT))
 font = pygame.font.Font(None, 36)
 
+# Counters for deviations and cooldown
+large_deviation_count = 0
+last_large_deviation_time = 0
+
+# Cooldown duration in seconds
+cooldown_duration = 3
+
+# Load the buzzer sound
+buzzer_sound = pygame.mixer.Sound("buzzer.wav")
+
 # Callback function to process the image and display it
 def process_image(image):
+    global large_deviation_count, last_large_deviation_time
+    
     image_data = process_img(image)
     image_surface = pygame.surfarray.make_surface(image_data.swapaxes(0, 1))
     screen.blit(image_surface, (0, 0))
@@ -64,15 +77,40 @@ def process_image(image):
     # Display deviation information
     current_lane = world.get_map().get_waypoint(vehicle.get_location())
     deviation = current_lane.transform.location.distance(vehicle.get_location())
-    deviation_text = "Deviation: {:.2f} m".format(deviation)
+    deviation_text = "Deviation: {:.2f}".format(deviation)
     deviation_color = (255, 255, 255)  # Default color
     
     if deviation > 0.9:
-        deviation_text += " - Large Deviation!"
-        deviation_color = (255, 0, 0)  # Red color for large deviations
+        current_time = time.time()
+        if current_time - last_large_deviation_time >= cooldown_duration:
+            deviation_text += " - Large Deviation!"
+            deviation_color = (255, 0, 0)  # Red color for large deviations
+            large_deviation_count += 1  # Increment the count of large deviations
+            last_large_deviation_time = current_time
+            
+            # Play the buzzer sound
+            buzzer_sound.play()
     
-    text_render = font.render(deviation_text, True, deviation_color)
-    screen.blit(text_render, (10, 10))
+    # Display deviation information and count
+    deviation_render = font.render(deviation_text, True, deviation_color)
+    count_text = "Large Deviations: {}".format(large_deviation_count)
+    count_color = (255, 0, 0) if large_deviation_count > 10 else (255, 255, 255)  # Red if > 10, otherwise white
+    count_render = font.render(count_text, True, count_color)
+    
+    screen.blit(deviation_render, (10, 10))
+    screen.blit(count_render, (10, 50))
+    
+    # Display "Impairment Detected" in the center of the screen if large deviations > 10
+    if large_deviation_count > 10:
+        impairment_text = "Impairment Detected"
+        impairment_render = font.render(impairment_text, True, (255, 0, 0))  # Red color
+        
+        # Calculate the position to center the text
+        text_width, text_height = font.size(impairment_text)
+        text_x = (IM_WIDTH - text_width) // 2
+        text_y = (IM_HEIGHT - text_height) // 2
+        
+        screen.blit(impairment_render, (text_x, text_y))
     
     pygame.display.flip()
 
